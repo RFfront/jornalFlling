abbb1e163bca83d7ba5a32c9817f2ff3479b294f
RFfront <goster5@yandex.ru>
2022-02-19 21:13:42 +0300

    socket add

--- jornalFlling/urls.py
+++ jornalFlling/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path(r'', include('socketio_app.urls')),
     path('', include('fillJ.urls')),
     path('admin/', admin.site.urls),
 ]
